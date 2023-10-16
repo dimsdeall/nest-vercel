35cfaab3bf9ea5a1723adc65ff25f146d191fd3f
Dimas Knitto <it08.knitto@gmail.com>
2023-10-16 14:08:03 +0700

    add vercel json3

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,14 @@ async function bootstrap() {
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
